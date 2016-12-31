7c2e0c13175cb56c5077d2cbcae207ee9e8e1f92
squeek <squeek502@hotmail.com>
2016-12-30 20:30:46 -0800

    Fix sending *way* too many saturation/exhaustion sync packets  - See https://github.com/squeek502/AppleCore/issues/82

--- java/squeek/appleskin/network/SyncHandler.java
+++ java/squeek/appleskin/network/SyncHandler.java
@@ -1,18 +1,19 @@
 package squeek.appleskin.network;
 
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.world.EnumDifficulty;
-import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.WorldTickEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 import squeek.appleskin.ModInfo;
 import squeek.appleskin.helpers.HungerHelper;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class SyncHandler
 {
@@ -30,8 +31,8 @@ public class SyncHandler
 	 * Sync saturation (vanilla MC only syncs when it hits 0)
 	 * Sync exhaustion (vanilla MC does not sync it at all)
 	 */
-	private float lastSaturationLevel = 0;
-	private float lastExhaustionLevel = 0;
+	private static final Map<UUID, Float> lastSaturationLevels = new HashMap<UUID, Float>();
+	private static final Map<UUID, Float> lastExhaustionLevels = new HashMap<UUID, Float>();
 
 	@SubscribeEvent
 	public void onLivingUpdateEvent(LivingUpdateEvent event)
@@ -40,18 +41,30 @@ public class SyncHandler
 			return;
 
 		EntityPlayerMP player = (EntityPlayerMP) event.getEntity();
+		Float lastSaturationLevel = lastSaturationLevels.get(player.getUniqueID());
+		Float lastExhaustionLevel = lastExhaustionLevels.get(player.getUniqueID());
 
-		if (this.lastSaturationLevel != player.getFoodStats().getSaturationLevel())
+		if (lastSaturationLevel == null || lastSaturationLevel != player.getFoodStats().getSaturationLevel())
 		{
 			CHANNEL.sendTo(new MessageSaturationSync(player.getFoodStats().getSaturationLevel()), player);
-			this.lastSaturationLevel = player.getFoodStats().getSaturationLevel();
+			lastSaturationLevels.put(player.getUniqueID(), player.getFoodStats().getSaturationLevel());
 		}
 
 		float exhaustionLevel = HungerHelper.getExhaustion(player);
-		if (Math.abs(this.lastExhaustionLevel - exhaustionLevel) >= 0.01f)
+		if (lastExhaustionLevel == null || Math.abs(lastExhaustionLevel - exhaustionLevel) >= 0.01f)
 		{
 			CHANNEL.sendTo(new MessageExhaustionSync(exhaustionLevel), player);
-			this.lastExhaustionLevel = exhaustionLevel;
+			lastExhaustionLevels.put(player.getUniqueID(), exhaustionLevel);
 		}
+	}
+
+	@SubscribeEvent
+	public void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event)
+	{
+		if (!(event.player instanceof EntityPlayerMP))
+			return;
+
+		lastSaturationLevels.remove(event.player.getUniqueID());
+		lastExhaustionLevels.remove(event.player.getUniqueID());
 	}
 }
