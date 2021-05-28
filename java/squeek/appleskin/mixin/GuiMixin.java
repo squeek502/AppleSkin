39240787366eedf92eed845fec8a0341a1c3da27
Ryan Liptak <squeek502@hotmail.com>
2021-05-28 14:56:07 -0700

    Move tooltip rendering after the rest of the tooltip has been rendered instead of before
    Fixes problems with alpha blending, Z depth testing, etc. We now draw our tooltip stuff after the rest of the tooltip has been drawn, which makes more sense overall.

--- java/squeek/appleskin/mixin/GuiMixin.java
+++ java/squeek/appleskin/mixin/GuiMixin.java
@@ -22,8 +22,25 @@ public class GuiMixin
 		TooltipOverlayHandler.onItemTooltip(itemStack, info.getReturnValue());
 	}
 
+	// captured before the tooltip is rendered and stored here temporarily
+	private static int x;
+	private static int y;
+	private static int w;
+	private static int h;
+
+	// capture x, y, etc at matrixStack.push()
 	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;push()V", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD, method = "renderOrderedTooltip(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;II)V")
-	private void renderTooltip(MatrixStack matrixStack, List tooltip, int mouseX, int mouseY, CallbackInfo info, int w, int x, int y, int w2, int h)
+	private void renderTooltipCaptureLocals(MatrixStack matrixStack, List tooltip, int mouseX, int mouseY, CallbackInfo info, int w, int x, int y, int w2, int h)
+	{
+		GuiMixin.x = x;
+		GuiMixin.y = y;
+		GuiMixin.w = w;
+		GuiMixin.h = h;
+	}
+
+	// do the actual rendering at matrixStack.pop()
+	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;pop()V", ordinal = 0), method = "renderOrderedTooltip(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;II)V")
+	private void renderTooltip(MatrixStack matrixStack, List tooltip, int mouseX, int mouseY, CallbackInfo info)
 	{
 		TooltipOverlayHandler.onRenderTooltip(matrixStack, tooltip, x, y, w, h);
 	}
