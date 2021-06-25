ef43c983c2dc849ae9d86b201e8ee1a4bea290d4
Ryan Liptak <squeek502@hotmail.com>
2021-06-24 22:04:07 -0700

    Update ModMenu dependency
    Maven location + package path were changed

--- java/squeek/appleskin/gui/ModMenuIntegration.java
+++ java/squeek/appleskin/gui/ModMenuIntegration.java
@@ -1,7 +1,7 @@
 package squeek.appleskin.gui;
 
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
