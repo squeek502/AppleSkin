eaea2c3d6af45ad4dd6743fd63cbcd28581ffc81
Ryan Liptak <squeek502@hotmail.com>
2021-06-29 00:33:06 -0700

    Add AppleSkinApi interface and "appleskin" entrypoint
    This allows mods to easily add integration with appleskin without having any dependency on AppleSkin beyond compile-time.

--- java/squeek/appleskin/AppleSkin.java
+++ java/squeek/appleskin/AppleSkin.java
@@ -1,14 +1,30 @@
 package squeek.appleskin;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import squeek.appleskin.api.AppleSkinApi;
 import squeek.appleskin.network.SyncHandler;
 
 public class AppleSkin implements ClientModInitializer
 {
+	public static final Logger LOGGER = LogManager.getLogger();
+
 	@Override
 	public void onInitializeClient()
 	{
 		SyncHandler.init();
 		ModConfig.init();
+		FabricLoader.getInstance().getEntrypointContainers("appleskin", AppleSkinApi.class).forEach(entrypoint -> {
+			try
+			{
+				entrypoint.getEntrypoint().registerEvents();
+			}
+			catch (Throwable e)
+			{
+				LOGGER.error("Failed to load entrypoint for mod {}", entrypoint.getProvider().getMetadata().getId(), e);
+			}
+		});
 	}
 }
