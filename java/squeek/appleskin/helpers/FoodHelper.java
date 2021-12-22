2677166a7f8723430550c63f5d34329d0222a675
Axel Rindle <mail@axelrindle.de>
2021-12-22 11:23:20 +0100

    Check the contained item

--- java/squeek/appleskin/helpers/FoodHelper.java
+++ java/squeek/appleskin/helpers/FoodHelper.java
@@ -16,7 +16,7 @@ public class FoodHelper
 {
 	public static boolean isFood(ItemStack itemStack)
 	{
-		if (itemStack == null)
+		if (itemStack.getItem() == null)
 			return false;
 
 		return itemStack.getItem().isFood();
