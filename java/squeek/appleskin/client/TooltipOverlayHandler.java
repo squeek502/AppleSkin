99c890468979baadd59b5e6c6177fd0e250600f1
Ryan Liptak <squeek502@hotmail.com>
2023-03-15 19:49:50 -0700

    Update for 1.19.4

--- java/squeek/appleskin/client/TooltipOverlayHandler.java
+++ java/squeek/appleskin/client/TooltipOverlayHandler.java
@@ -192,10 +192,10 @@ public class TooltipOverlayHandler
 		}
 
 		@Override
-		public void drawItems(TextRenderer textRenderer, int x, int y, MatrixStack matrices, ItemRenderer itemRenderer, int z)
+		public void drawItems(TextRenderer textRenderer, int x, int y, MatrixStack matrices, ItemRenderer itemRenderer)
 		{
 			if (TooltipOverlayHandler.INSTANCE != null)
-				TooltipOverlayHandler.INSTANCE.onRenderTooltip(matrices, this, x, y, z, textRenderer);
+				TooltipOverlayHandler.INSTANCE.onRenderTooltip(matrices, this, x, y, 0, textRenderer);
 		}
 	}
 
