fa04d7a33194d665c25ce7d315195d14aab84cc0
HhiPyrene <111572269+HhiPyrene@users.noreply.github.com>
2023-08-06 21:04:20 +0800

    [Fabric] Translable config tooltips + zh_cn

--- java/squeek/appleskin/ModConfig.java
+++ java/squeek/appleskin/ModConfig.java
@@ -5,7 +5,6 @@ import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 
 @Config(name = "appleskin")
@@ -21,34 +20,34 @@ public class ModConfig implements ConfigData
 		INSTANCE = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
 	}
 
-	@Comment("If true, shows the hunger and saturation values of food in its tooltip while holding SHIFT")
+	@ConfigEntry.Gui.Tooltip()
 	public boolean showFoodValuesInTooltip = true;
 
-	@Comment("If true, shows the hunger and saturation values of food in its tooltip automatically (without needing to hold SHIFT)")
+	@ConfigEntry.Gui.Tooltip()
 	public boolean showFoodValuesInTooltipAlways = true;
 
-	@Comment("If true, shows your current saturation level overlayed on the hunger bar")
+	@ConfigEntry.Gui.Tooltip()
 	public boolean showSaturationHudOverlay = true;
 
-	@Comment("If true, shows the hunger (and saturation if showSaturationHudOverlay is true) that would be restored by food you are currently holding")
+	@ConfigEntry.Gui.Tooltip()
 	public boolean showFoodValuesHudOverlay = true;
 
-	@Comment("If true, enables the hunger/saturation/health overlays for food in your off-hand")
+	@ConfigEntry.Gui.Tooltip()
 	public boolean showFoodValuesHudOverlayWhenOffhand = true;
 
-	@Comment("If true, shows your food exhaustion as a progress bar behind the hunger bar")
+	@ConfigEntry.Gui.Tooltip()
 	public boolean showFoodExhaustionHudUnderlay = true;
 
-	@Comment("If true, shows estimated health restored by food on the health bar")
+	@ConfigEntry.Gui.Tooltip()
 	public boolean showFoodHealthHudOverlay = true;
 
-	@Comment("If true, shows your hunger, saturation, and exhaustion level in Debug Screen")
+	@ConfigEntry.Gui.Tooltip()
 	public boolean showFoodDebugInfo = true;
 
-	@Comment("If true, health/hunger overlay will shake to match Minecraft's icon animations")
+	@ConfigEntry.Gui.Tooltip()
 	public boolean showVanillaAnimationsOverlay = true;
 
-	@Comment("Alpha value of the flashing icons at their most visible point (1.0 = fully opaque, 0.0 = fully transparent)")
+	@ConfigEntry.Gui.Tooltip()
 	public float maxHudOverlayFlashAlpha = 0.65f;
 }
 
