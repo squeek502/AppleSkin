a36ca84412f66f23f767efda6346da750b887c8c
Ryan Liptak <squeek502@hotmail.com>
2023-09-26 20:14:57 -0700

    Backport latest lang files + mod config tooltip localization

--- java/squeek/appleskin/ModConfig.java
+++ java/squeek/appleskin/ModConfig.java
@@ -21,33 +21,43 @@ public class ModConfig implements ConfigData
 		INSTANCE = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
 	}
 
+	@ConfigEntry.Gui.Tooltip()
 	@Comment("If true, shows the hunger and saturation values of food in its tooltip while holding SHIFT")
 	public boolean showFoodValuesInTooltip = true;
 
+	@ConfigEntry.Gui.Tooltip()
 	@Comment("If true, shows the hunger and saturation values of food in its tooltip automatically (without needing to hold SHIFT)")
 	public boolean showFoodValuesInTooltipAlways = true;
 
+	@ConfigEntry.Gui.Tooltip()
 	@Comment("If true, shows your current saturation level overlayed on the hunger bar")
 	public boolean showSaturationHudOverlay = true;
 
+	@ConfigEntry.Gui.Tooltip()
 	@Comment("If true, shows the hunger (and saturation if showSaturationHudOverlay is true) that would be restored by food you are currently holding")
 	public boolean showFoodValuesHudOverlay = true;
 
+	@ConfigEntry.Gui.Tooltip()
 	@Comment("If true, enables the hunger/saturation/health overlays for food in your off-hand")
 	public boolean showFoodValuesHudOverlayWhenOffhand = true;
 
+	@ConfigEntry.Gui.Tooltip()
 	@Comment("If true, shows your food exhaustion as a progress bar behind the hunger bar")
 	public boolean showFoodExhaustionHudUnderlay = true;
 
+	@ConfigEntry.Gui.Tooltip()
 	@Comment("If true, shows estimated health restored by food on the health bar")
 	public boolean showFoodHealthHudOverlay = true;
 
+	@ConfigEntry.Gui.Tooltip()
 	@Comment("If true, shows your hunger, saturation, and exhaustion level in Debug Screen")
 	public boolean showFoodDebugInfo = true;
 
+	@ConfigEntry.Gui.Tooltip()
 	@Comment("If true, health/hunger overlay will shake to match Minecraft's icon animations")
 	public boolean showVanillaAnimationsOverlay = true;
 
+	@ConfigEntry.Gui.Tooltip()
 	@Comment("Alpha value of the flashing icons at their most visible point (1.0 = fully opaque, 0.0 = fully transparent)")
 	public float maxHudOverlayFlashAlpha = 0.65f;
 }
