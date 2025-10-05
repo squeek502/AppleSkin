ea7e8ef4fdb3e88c9cee7c59c2d5e6e8869e1027
AmirAli Mollaei <me.amiralimollaei@gmail.com>
2025-10-05 13:46:33 +0330

    Properly implement `DebugHudEntry`

---
+++ java/squeek/appleskin/client/DebugInfoHudEntry.java
@@ -0,0 +1,53 @@
+package squeek.appleskin.client;
+
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.debug.*;
+import net.minecraft.entity.player.HungerManager;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.WorldChunk;
+import org.jetbrains.annotations.Nullable;
+import squeek.appleskin.ModConfig;
+import squeek.appleskin.helpers.ExhaustionHelper;
+import squeek.appleskin.helpers.FoodHelper;
+
+import java.text.DecimalFormat;
+
+public class DebugInfoHandler implements DebugHudEntry
+{
+	public static final Identifier ENTRY_ID = Identifier.of("appleskin", "debuginfo");
+	public static final Identifier SECTION_ID = Identifier.ofVanilla("position");
+
+	private static final DecimalFormat saturationDF = new DecimalFormat("#.##");
+	private static final DecimalFormat exhaustionValDF = new DecimalFormat("0.00");
+	private static final DecimalFormat exhaustionMaxDF = new DecimalFormat("#.##");
+
+	@Override
+	public void render(
+			DebugHudLines lines,
+			@Nullable World world,
+			@Nullable WorldChunk clientChunk,
+			@Nullable WorldChunk chunk
+	) {
+		if (world != null) {
+			MinecraftClient mc = MinecraftClient.getInstance();
+			if (mc == null || mc.player == null)
+				return;
+
+			HungerManager stats = mc.player.getHungerManager();
+			if (stats == null) {
+				return;
+			}
+
+			float curExhaustion = ExhaustionHelper.getExhaustion(mc.player);
+			float maxExhaustion = FoodHelper.MAX_EXHAUSTION;
+			lines.addLineToSection(SECTION_ID, "hunger: " + stats.getFoodLevel() + ", sat: " + saturationDF.format(stats.getSaturationLevel()) + ", exh: " + exhaustionValDF.format(curExhaustion) + "/" + exhaustionMaxDF.format(maxExhaustion));
+		}
+	}
+
+	@Override
+	public boolean canShow(boolean reducedDebugInfo) {
+        return ModConfig.INSTANCE.showFoodDebugInfo;
+    }
+}
