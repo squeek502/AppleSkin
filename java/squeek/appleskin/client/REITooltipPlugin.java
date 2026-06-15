7c7d9ee3569ffe10196a515208f64d93249f7999
Ryan Liptak <squeek502@hotmail.com>
2026-06-15 14:33:21 -0700

    Reinstate REI compat
    Fixes #441

--- java/squeek/appleskin/client/REITooltipPlugin.java
+++ java/squeek/appleskin/client/REITooltipPlugin.java
@@ -1,7 +1,5 @@
 package squeek.appleskin.client;
 
-// TODO: Reinstate once REI for 26.1 is released
-/*
 import me.shedaniel.rei.api.client.entry.renderer.EntryRendererRegistry;
 import me.shedaniel.rei.api.client.gui.widgets.Tooltip;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
@@ -49,4 +47,3 @@ public class REITooltipPlugin implements REIClientPlugin
 		});
 	}
 }
-*/
